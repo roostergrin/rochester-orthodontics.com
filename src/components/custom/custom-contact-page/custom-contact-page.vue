5e6f5322145cebc2d1ebd1a7740e539d0345c53a
luketevers <luketevers@gmail.com>
2019-01-10 09:58:51 -0800

    refactor(custom-contact-page): adds modal for emergencies

--- src/components/custom/custom-contact-page/custom-contact-page.vue
+++ src/components/custom/custom-contact-page/custom-contact-page.vue
@@ -5,6 +5,22 @@ import MainForm from 'components/form/form'
 
 export default {
   props: ['content'],
+  data () {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal () {
+      this.showModal = !this.showModal
+      this.showModal ? document.body.classList.add('body-stop') : document.body.classList.remove('body-stop')
+      if (this.showModal) {
+        setTimeout(() => {
+          this.slideWidth = this.$refs.slider.clientWidth / 3
+        }, 100)
+      }
+    }
+  },
   components: {
     MainForm
   }
