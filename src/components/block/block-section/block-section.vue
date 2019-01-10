ac1b0e61575d8dc49186ab3028719289159413f9
luketevers <luketevers@gmail.com>
2019-01-09 17:57:44 -0800

    feat(block-section): adds modal slider;

--- src/components/block/block-section/block-section.vue
+++ src/components/block/block-section/block-section.vue
@@ -15,6 +15,48 @@ export default {
         return false
       }
     }
+  },
+  data () {
+    return {
+      showModal: false,
+      current: 0,
+      showing: 0,
+      shifting: false,
+      slideWidth: null,
+      slides: null
+    }
+  },
+  mounted () {
+    let modalInterval = setInterval(() => {
+      if (this.content.modal !== undefined) {
+        this.slides = this.content.modal.length
+        clearInterval(modalInterval)
+      }
+    }, 100)
+  },
+  methods: {
+    switchSlide () {
+      if (this.showing >= this.content.modal.length - 3) {
+        if (this.current >= this.content.modal.length - 1) {
+          this.showing = 0
+          this.current = 0
+        } else {
+          this.current += 1
+        }
+      } else {
+        this.showing += 1
+        this.current += 1
+      }
+    },
+    toggleModal () {
+      this.showModal = !this.showModal
+      this.showModal ? document.body.classList.add('body-stop') : document.body.classList.remove('body-stop')
+      if (this.showModal) {
+        setTimeout(() => {
+          this.slideWidth = this.$refs.slider.clientWidth / 3
+        }, 100)
+      }
+    }
   }
 }
 </script>
