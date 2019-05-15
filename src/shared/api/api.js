52b431a42821b84d9a5a1d102002364bb9ee16c2
juancarlos <juancarlos@roostergrin.com>
2019-05-15 11:30:39 -0700

    fixes 502 gateway error

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -1,11 +1,7 @@
-const route = '//www.abbottorthodontics.com/wp-json'
+const route = 'https//www.rochester-orthodontics.com/wp-json'
 
 const api = () => {
-  try {
-    return (document.location.protocol === 'https:' ? 'https:' : 'http:') + route
-  } catch (e) {
-    return 'https://' + route
-  }
+  return route
 }
 
 export default api()
