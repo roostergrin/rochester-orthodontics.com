b2648600320847aefb1ea4a3a83da0f9e2d74f60
juancarlos <juancarlos@roostergrin.com>
2019-05-15 11:56:58 -0700

    uppdates(fonts.js)removes conditional src, and leaves only https

--- src/resources/fonts.js
+++ src/resources/fonts.js
@@ -3,9 +3,9 @@ const fonts = () => {
   var WebFontConfig = {
     google: { families: [ 'Open+Sans:400,600' ] }
   }
-  
+
   var wf = document.createElement('script')
-  wf.src = (document.location.protocol === 'https' ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'
+  wf.src = (document.location.protocol === 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'
   wf.type = 'text/javascript'
   wf.async = 'true'
   var s = document.getElementsByTagName('script')[0]
