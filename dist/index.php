ccab134664bcb1beb6afac88cc32d829506773bb
GreenWebMedia <37212563+GreenWebMedia@users.noreply.github.com>
2021-06-29 10:46:53 +0530

    SEO Code Implemented

--- dist/index.php
+++ dist/index.php
@@ -3,6 +3,40 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+     <!-- Global site tag (gtag.js) - Google Ads: 409032805 -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-409032805"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'AW-409032805');
+    </script>	
+
+    <script>
+      gtag('config', 'AW-409032805/9epjCNWDhoACEOWwhcMB', {
+        'phone_conversion_number': '248.656.0040'
+      });
+    </script>
+
+    <?php $actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+      if($actual_link=='https://rochester-orthodontics.com/thank-you/'){ ?>
+    <!-- Event snippet for Rochester Ortho - Submit lead form conversion page -->
+    <script>
+      gtag('event', 'conversion', {'send_to': 'AW-409032805/c8UYCJjd_P8BEOWwhcMB'});
+    </script>
+
+
+    <?php  } ?> 
+    <?php $actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+      if($actual_link=='https://rochester-orthodontics.com/thank-you'){ ?>
+    <!-- Event snippet for Rochester Ortho - Submit lead form conversion page -->
+    <script>
+      gtag('event', 'conversion', {'send_to': 'AW-409032805/c8UYCJjd_P8BEOWwhcMB'});
+    </script>
+
+    <?php  } ?>	 
     <?php wp_head(); ?>
     <meta name='google-site-verification' content='RcXvXY-WaMuwK4zeIjPPyd8cRlh1pCp_4wmPkaCQXGo' />
   <link rel='stylesheet' id='sb-font-awesome-css' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css' media='all' />    
